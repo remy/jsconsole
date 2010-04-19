dc3426266407196d5978433c78605ef9dfdf3250
Remy Sharp <remy@leftlogic.com>
2010-04-19 10:23:49 +0100

    Added credit, supports tabs in main window, fixed a few key strokes and support clear via cmd+shift+del

--- console.js
+++ console.js
@@ -55,6 +55,7 @@ function run(cmd) {
 }
 
 function post(cmd) {
+  cmd = trim(cmd);
   history.push(cmd);
   
   // order so it appears at the top  
@@ -73,7 +74,7 @@ function post(cmd) {
   }
   
   output.parentNode.scrollTop = 0;
-  exec.value = '';
+  if (!body.className) exec.value = '';
   pos = history.length;
 }
 
@@ -87,6 +88,73 @@ function changeView(event){
     exec.focus();
     return false;
   }
+}
+
+function checkTab(evt) {
+  var t = evt.target,
+      ss = t.selectionStart,
+      se = t.selectionEnd,
+      tab = "  ";
+  
+
+  // Tab key - insert tab expansion
+  if (evt.keyCode == 9) {
+    evt.preventDefault();
+
+    // Special case of multi line selection
+    if (ss != se && t.value.slice(ss,se).indexOf("\n") != -1) {
+      // In case selection was not of entire lines (e.g. selection begins in the middle of a line)
+      // we ought to tab at the beginning as well as at the start of every following line.
+      var pre = t.value.slice(0,ss);
+      var sel = t.value.slice(ss,se).replace(/\n/g,"\n"+tab);
+      var post = t.value.slice(se,t.value.length);
+      t.value = pre.concat(tab).concat(sel).concat(post);
+
+      t.selectionStart = ss + tab.length;
+      t.selectionEnd = se + tab.length;
+    }
+
+    // "Normal" case (no selection or selection on one line only)
+    else {
+      t.value = t.value.slice(0,ss).concat(tab).concat(t.value.slice(ss,t.value.length));
+      if (ss == se) {
+        t.selectionStart = t.selectionEnd = ss + tab.length;
+      }
+      else {
+        t.selectionStart = ss + tab.length;
+        t.selectionEnd = se + tab.length;
+      }
+    }
+  }
+
+  // Backspace key - delete preceding tab expansion, if exists
+  else if (evt.keyCode==8 && t.value.slice(ss - 4,ss) == tab) {
+    evt.preventDefault();
+
+    t.value = t.value.slice(0,ss - 4).concat(t.value.slice(ss,t.value.length));
+    t.selectionStart = t.selectionEnd = ss - tab.length;
+  }
+
+  // Delete key - delete following tab expansion, if exists
+  else if (evt.keyCode==46 && t.value.slice(se,se + 4) == tab) {
+    evt.preventDefault();
+
+    t.value = t.value.slice(0,ss).concat(t.value.slice(ss + 4,t.value.length));
+    t.selectionStart = t.selectionEnd = ss;
+  }
+  // Left/right arrow keys - move across the tab in one go
+  else if (evt.keyCode == 37 && t.value.slice(ss - 4,ss) == tab) {
+    evt.preventDefault();
+    t.selectionStart = t.selectionEnd = ss - 4;
+  }
+  else if (evt.keyCode == 39 && t.value.slice(ss,ss + 4) == tab) {
+    evt.preventDefault();
+    t.selectionStart = t.selectionEnd = ss + 4;
+  }
+}
+
+function trim(s) {
+  return (s||"").replace(/^\s+|\s+$/g,"");
 }
 
 var _console = {
@@ -116,11 +184,12 @@ sandbox.write('<script>var console = ' + stringify(_console).replace(/\\n/g, ' '
 sandbox.close();
 
 exec.onkeydown = function (event) {
-  var keys = {38:1, 40:1};
+  event = event || window.event;
+  var keys = {38:1, 40:1}, wide = body.className == 'large';
   if (keys[event.which]) {
     if (event.shiftKey) {
       changeView(event);
-    } else {
+    } else if (!wide) {
       if (event.which == 38) { // cycle up
         pos--;
         if (pos < 0) pos = history.length - 1;
@@ -132,13 +201,17 @@ exec.onkeydown = function (event) {
         exec.value = history[pos];
         // event.preventDefault && event.preventDefault();
         return false;
-      }      
+      }
     }
   } else if (event.which == 13 || event.which == 10) { // enter (what about the other one)
-    if (event.shiftKey == true || event.metaKey || event.ctrlKey || !body.className) {
+    if (event.shiftKey == true || event.metaKey || event.ctrlKey || !wide) {
       post(exec.value);
-      return false;      
+      return false;
     }
+  } else if (event.which == 9 && wide) {
+    checkTab(event);
+  } else if (event.shiftKey && event.metaKey && event.which == 8) {
+    output.innerHTML = '';
   }
 };
 
@@ -151,8 +224,13 @@ form.onsubmit = function (event) {
 
 document.onkeydown = function (event) {
   event = event || window.event;
+  
+  if (event.shiftKey && event.metaKey && event.which == 8) {
+    output.innerHTML = '';
+    exec.focus();
+  }
   return changeView(event);
-}
+};
 
 if (window.location.search) {
   post(decodeURIComponent(window.location.search.substr(1)));
