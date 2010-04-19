72b7923d3d972d8b32d94b757cc5e8a87673bcdc
remy <remy@remysharp.com>
2010-04-19 11:53:55 +0100

    More ie support

--- console.js
+++ console.js
@@ -100,8 +100,6 @@ function checkTab(evt) {
 
   // Tab key - insert tab expansion
   if (evt.keyCode == 9) {
-    evt.preventDefault();
-
     // Special case of multi line selection
     if (ss != se && t.value.slice(ss,se).indexOf("\n") != -1) {
       // In case selection was not of entire lines (e.g. selection begins in the middle of a line)
@@ -130,28 +128,25 @@ function checkTab(evt) {
 
   // Backspace key - delete preceding tab expansion, if exists
   else if (evt.keyCode==8 && t.value.slice(ss - 4,ss) == tab) {
-    evt.preventDefault();
-
     t.value = t.value.slice(0,ss - 4).concat(t.value.slice(ss,t.value.length));
     t.selectionStart = t.selectionEnd = ss - tab.length;
   }
 
   // Delete key - delete following tab expansion, if exists
   else if (evt.keyCode==46 && t.value.slice(se,se + 4) == tab) {
-    evt.preventDefault();
-
     t.value = t.value.slice(0,ss).concat(t.value.slice(ss + 4,t.value.length));
     t.selectionStart = t.selectionEnd = ss;
   }
   // Left/right arrow keys - move across the tab in one go
   else if (evt.keyCode == 37 && t.value.slice(ss - 4,ss) == tab) {
-    evt.preventDefault();
     t.selectionStart = t.selectionEnd = ss - 4;
   }
   else if (evt.keyCode == 39 && t.value.slice(ss,ss + 4) == tab) {
-    evt.preventDefault();
     t.selectionStart = t.selectionEnd = ss + 4;
+  } else {
+    return true;
   }
+  return false;
 }
 
 function trim(s) {
