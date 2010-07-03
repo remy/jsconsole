e9888e886e95d181d5244431ab0df9eca1c380f9
remy <remy@remysharp.com>
2010-07-03 23:32:54 +0100

    Playing with injection version of jsconsole

--- inject.js
+++ inject.js
@@ -6,26 +6,36 @@
 
   var body = document.body;
 
-  var container = document.createElement('div');
-  container.id = 'jsconsole';
-  container.innerHTML = '<form><textarea autofocus id="exec" spellcheck="false" autocapitalize="off" rows="1"></textarea></form><div id="console"><ul id="output"></ul></div>';
-  body.appendChild(container);
-  container.style.position = 'absolute';
-  container.style.top = '0px';
-  container.style.left = '0px';
-  container.style.width = '100%';
+  var iframe = document.createElement('iframe'),
+      doc = iframe.contentDocument || iframe.contentWindow.document;
+      
+  iframe.style.display = 'none';
+  body.appendChild(iframe);
+  
+  doc.open();
+  doc.write('<!DOCTYPE html><html id="jsconsole"><head><title>jsconsole</title><link rel="stylesheet" href="http://jsconsole.com/console.css" type="text/css" /></head><body><form><textarea autofocus id="exec" spellcheck="false" autocapitalize="off" rows="1"></textarea></form><div id="console"><ul id="output"></ul></div><script src="http://jsconsole.com/prettify.js"></script><script src="http://jsconsole.com/console.js"></script></body></html>');
+  doc.close();
+  
+  // container.id = 'jsconsole';
+  // container.innerHTML = '<form><textarea autofocus id="exec" spellcheck="false" autocapitalize="off" rows="1"></textarea></form><div id="console"><ul id="output"></ul></div>';
+  // body.appendChild(container);
+  iframe.style.position = 'absolute';
+  iframe.style.top = '0px';
+  iframe.style.left = '0px';
+  iframe.style.width = '100%';
+  iframe.style.height = '100%';
 
-  var link = document.createElement('link');
-  link.type = 'text/css';
-  link.rel = 'stylesheet';
-  link.href = 'http://jsconsole.com/console.css';
-  body.appendChild(link);
-
-  var s1 = document.createElement('script');
-  s1.src = 'http://jsconsole.com/prettify.js';
-  body.appendChild(s1);
-
-  var s2 = document.createElement('script');
-  s2.src = 'http://jsconsole.com/console.js';
-  body.appendChild(s2);  
+  // var link = document.createElement('link');
+  // link.type = 'text/css';
+  // link.rel = 'stylesheet';
+  // link.href = 'http://jsconsole.com/console.css';
+  // body.appendChild(link);
+  // 
+  // var s1 = document.createElement('script');
+  // s1.src = 'http://jsconsole.com/prettify.js';
+  // body.appendChild(s1);
+  // 
+  // var s2 = document.createElement('script');
+  // s2.src = 'http://jsconsole.com/console.js';
+  // body.appendChild(s2);  
 })();
