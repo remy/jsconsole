cb3de47476b37c2dc907496d72fb32ad9bafc2d4
remy <remy@remysharp.com>
2010-07-03 23:52:02 +0100

    Removed offline refs

--- inject.js
+++ inject.js
@@ -27,7 +27,7 @@
     doc = iframe.contentDocument || iframe.contentWindow.document;
 
     doc.open();
-    doc.write('<!DOCTYPE html><html id="jsconsole"><head><title>jsconsole</title><link rel="stylesheet" href="http://offline.jsconsole.com/console.css" type="text/css" /></head><body><form><textarea autofocus id="exec" spellcheck="false" autocapitalize="off" autofocus rows="1"></textarea></form><div id="console"><ul id="output"></ul></div><div id="footer"><a href="http://github.com/remy/console">Fork on Github</a> &bull; <a href="http://twitter.com/rem">Built by @rem</a></div><script src="http://offline.jsconsole.com/prettify.js"></script><script src="http://offline.jsconsole.com/console.js?' + Math.random() + '"></script></body></html>');
+    doc.write('<!DOCTYPE html><html id="jsconsole"><head><title>jsconsole</title><link rel="stylesheet" href="http://jsconsole.com/console.css" type="text/css" /></head><body><form><textarea autofocus id="exec" spellcheck="false" autocapitalize="off" autofocus rows="1"></textarea></form><div id="console"><ul id="output"></ul></div><div id="footer"><a href="http://github.com/remy/console">Fork on Github</a> &bull; <a href="http://twitter.com/rem">Built by @rem</a></div><script src="http://jsconsole.com/prettify.js"></script><script src="http://jsconsole.com/console.js?' + Math.random() + '"></script></body></html>');
     doc.close();
     
     iframe.contentWindow.onload = function () {
