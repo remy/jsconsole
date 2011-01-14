0f955f5d6543e5b950312f1231049cb53059c0b2
remy <remy@remysharp.com>
2011-01-14 16:40:42 +0000

    Fixed the right cursor to move to end - via StackOverflow

--- console.js
+++ console.js
@@ -54,7 +54,7 @@ function stringify(o, simple) {
 }
 
 function cleanse(s) {
-  return s.replace(/[<>&]/g, function (m) { return {'&':'&amp;','>':'&gt;','<':'&lt;'}[m];});
+  return (s||'').replace(/[<>&]/g, function (m) { return {'&':'&amp;','>':'&gt;','<':'&lt;'}[m];});
 }
 
 function run(cmd) {
@@ -694,6 +694,21 @@ function completeCode(focus) {
   // move the cursor to the end of the text.
   document.getElementsByTagName('a')[0].focus();
   cursor.focus();
+  
+  var range, selection;
+  if (document.createRange) {//Firefox, Chrome, Opera, Safari, IE 9+
+    range = document.createRange();//Create a range (a range is a like the selection but invisible)
+    range.selectNodeContents(cursor);//Select the entire contents of the element with the range
+    range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+    selection = window.getSelection();//get the selection object (allows you to change selection)
+    selection.removeAllRanges();//remove any selections already made
+    selection.addRange(range);//make the range you have just created the visible selection
+  } else if (document.selection) {//IE 8 and lower
+    range = document.body.createTextRange();//Create a range (a range is a like the selection but invisible)
+    range.moveToElementText(cursor);//Select the entire contents of the element with the range
+    range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+    range.select();//Select the range (make it the visible selection
+  }
 }
 
 form.onsubmit = function (event) {
