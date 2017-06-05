fb91a841f26454c06ff0dbec813fb0271a1e2037
Remy Sharp <remy@remysharp.com>
2017-06-05 19:25:55 +0100

    fix: filtering on console output

--- src/components/Line.js
+++ src/components/Line.js
@@ -41,30 +41,28 @@ class Line extends Component {
         </div>);
     }
 
-    if (type === 'log') {
+    if (type === 'log' || type === 'response') {
       line = (
         <div className={`prompt output ${type} ${error ? 'error' : ''}`}>
-          <LineNav value={value} command={command} />
-        {
-          (Array.isArray(value) ? value : [value]).map((value, i) => {
-            const Type = which(value);
-            return <Type styled={styled} bare={true} key={`type-${i}`} allowOpen={true} value={value} shallow={false} open={open}>{ value }</Type>;
-          })
-        }
-        </div>
-      );
-    }
-
-    if (type === 'response') {
-      const Type = which(value);
-      line = (
-        <div className={`prompt output ${type} ${error ? 'error' : ''}`}>
-
           <LineNav onFilter={filter => {
             this.setState({ filter });
           }} value={value} command={command} />
 
-          <Type allowOpen={true} value={value} filter={filter} shallow={false} open={open}>{ value }</Type>
+          {
+            (Array.isArray(value) ? value : [value]).map((value, i) => {
+              const Type = which(value);
+              return <Type
+                filter={filter}
+                styled={styled}
+                value={value}
+                open={open}
+                allowOpen={true}
+                bare={type === 'log'}
+                key={`type-${i}`}
+                shallow={false}
+              >{ value }</Type>;
+            })
+          }
         </div>
       );
     }
