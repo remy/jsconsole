b2ddb943f86943f27345594d53873def5f6e5896
Remy Sharp <remy@remysharp.com>
2017-07-26 16:01:06 +0100

    fix: app css and allow setting container

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -65,7 +65,7 @@ module.exports = function(proxy, allowedHost) {
     // Reportedly, this avoids CPU overload on some systems.
     // https://github.com/facebookincubator/create-react-app/issues/293
     watchOptions: {
-      ignored: /node_modules/,
+      // ignored: /node_modules/,
     },
     // Enable HTTPS if the HTTPS environment variable is set to 'true'
     https: protocol === 'https',
